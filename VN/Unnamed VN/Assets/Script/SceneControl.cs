9cd7baf9104efcd5210927941eb19f225643c5bc
Ziwei Ye <calvin@yuugu.re>
2016-10-12 20:10:40 -0400

    init

---
+++ VN/Unnamed VN/Assets/Script/SceneControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SceneControl : MonoBehaviour {
+
+    public GameObject bg;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void ChangeBG(string path) {
+        Texture2D texture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Resources/Textures/" + path, typeof(Texture2D));
+        bg.GetComponent<Renderer>().material.mainTexture = texture;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("b")) ChangeBG("test2.jpg");
+	}
+}
