af3f2a5c5b4f369c72d294709c93117d100f1cd2
Ziwei Ye <calvin@yuugu.re>
2016-10-22 20:04:19 -0400

    BG resolution works

--- VN/Unnamed VN/Assets/Script/SceneControl.cs
+++ VN/Unnamed VN/Assets/Script/SceneControl.cs
@@ -12,7 +12,10 @@ public class SceneControl : MonoBehaviour {
 
     void ChangeBG(string path) {
         Texture2D texture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Resources/Textures/" + path, typeof(Texture2D));
-        bg.GetComponent<Renderer>().material.mainTexture = texture;
+        Rect rec = new Rect(0, 0, texture.width, texture.height);
+        bg.GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, rec, new Vector2(0, 0), 1);
+        //Texture2D texture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Resources/Textures/" + path, typeof(Texture2D));
+        //bg.GetComponent<Renderer>().material.mainTexture = texture;
     }
 
     // Update is called once per frame
---
+++ VN/Unnamed VN/Assets/Script/TachieManage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TachieManage : MonoBehaviour {
+
+    // Use this for initialization
+
+    void addTachie() {
+        //gameObject c = new Cub
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
