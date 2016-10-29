78181b89dbb199ad520de69472132fc31e123b7a
Joey Tong <Joey Tong>
2016-10-29 17:34:40 -0400

    Dialogue Updates

---
+++ VN/Unnamed VN/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueManager : MonoBehaviour {
+    public Text dialogueBox;
+    string bufferText;
+    float timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        SetText("Time:" + timer + " seconds");
+	}
+    void SetText(string s)
+    {
+        dialogueBox.text = s;
+    }
+    void DisplayText()
+    {
+        
+    }
+    void AppendText(string s)
+    {
+        
+    }
+}
