b7e77ab884c2241c304b007b6ca22e5197fa1f23
Raymond To <Raymond To>
2016-11-12 00:09:10 -0500

    UI Updated

---
+++ VN/Unnamed VN/Assets/Scripts/UI Scripts/InGameMenuManger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenuManger : MonoBehaviour {
+    public Menu CurrentMenu; //InGame Menu
+    public Menu MainMenu; //menu triggered by ESC
+
+    public void Start()
+    {
+        //ShowMenu(CurrentMenu); //set InGame Menu start at begin
+    }
+
+    public void ShowMenu(Menu menu) //menu depend on On Click() send
+    {
+        //close current menu, open menu depend on On Click()
+        if (CurrentMenu != null)
+            CurrentMenu.IsOpen = false; //(menu.cs) return value to IsOpen
+
+        CurrentMenu = menu; // menu.cs that trigger animation
+        CurrentMenu.IsOpen = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        //if (Input.GetKeyDown("escape"))
+        {
+            MainMenu.IsOpen = true;
+            CurrentMenu.IsOpen = false;
+            Debug.Log("You have enter the ESC key!");
+        }
+    }
+}
---
+++ VN/Unnamed VN/Assets/Scripts/UI Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+    public Menu CurrentMenu; //InGame Menu
+    public Menu previousMenu; //menu triggered by ESC
+
+    public void Start()
+    {
+        ShowMenu(CurrentMenu); //set InGame Menu start at begin
+    }
+
+    public void ShowMenu(Menu menu) //menu depend on On Click() send
+    {
+        //close current menu, open menu depend on On Click()
+        if (CurrentMenu != null)
+            CurrentMenu.IsOpen = false; //(menu.cs) return value to IsOpen
+        
+            previousMenu = CurrentMenu;
+        CurrentMenu = menu; // menu.cs that trigger animation
+        CurrentMenu.IsOpen = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        //if (Input.GetKeyDown("escape"))
+        {
+            if (previousMenu != null)
+                previousMenu.IsOpen = true;
+            CurrentMenu.IsOpen = false;
+            Debug.Log("You have enter the ESC key!");
+        }
+    }
+}
