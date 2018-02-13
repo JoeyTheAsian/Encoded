49698639b81c37ea347c4fab287682645e1a9017
Joey Tong (RIT Student) <jpt1447@ad.rit.edu>
2018-02-13 12:01:00 -0500

    Confirmation Dialog

---
+++ VN/Unnamed VN/Assets/ConfirmationDialog.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmationDialog : MonoBehaviour {
+    public Button okButton;
+    public Button cancelButton;
+    public Text displayText;
+    public GameObject container;
+
+    public string textFieldData = "Name";
+    public bool displayTextField = false;
+    void OnGUI()
+    {
+        if (displayTextField)
+        {
+            Vector2 pos = new Vector2(Screen.width/2f, Screen.height/2f);
+            float width = gameObject.GetComponent<RectTransform>().rect.width;
+            float height = gameObject.GetComponent<RectTransform>().rect.height;
+            textFieldData = GUI.TextField(new Rect(pos.x - 100, pos.y - 10, 200, 20), textFieldData, 25);
+        }
+    }
+    public void Display()
+    {
+        container.SetActive(container.activeSelf ? false : true);
+    }
+    public void Hide()
+    {
+        container.SetActive(false);
+    }
+    public void ButtonPress()
+    {
+        Hide();
+    }
+}
